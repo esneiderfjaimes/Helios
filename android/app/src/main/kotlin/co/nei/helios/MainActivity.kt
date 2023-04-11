0278361c9174aaf18bb5ba2b7f901fad897654c3
Esneider Jaimes <62038623+esneiderfjaimes@users.noreply.github.com>
2023-04-11 08:30:45 -0500

    base app

---
+++ android/app/src/main/kotlin/co/nei/helios/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.nei.helios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Helios App',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+        ),
+        themeMode: ThemeMode.system,
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hi Helios.',
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+    );
+  }
+}
